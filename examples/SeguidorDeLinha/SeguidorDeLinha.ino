d473200fcaed7d014e1e0a5e2d3f9af542bab875
Victor Chacon <victor.codesseira@gmail.com>
2017-10-02 15:16:44 -0300

    Remove sensors array

--- examples/SeguidorDeLinha/SeguidorDeLinha.ino
+++ examples/SeguidorDeLinha/SeguidorDeLinha.ino
@@ -1,8 +1,9 @@
 #include "ThunderKit.h"
 
+#define BLACK 0
+#define WHITE 1
 
 ThunderKit kit(1);
-int i, sensores[5];
 
 void setup() {
 	if (kit.begin() != 0) {
@@ -13,24 +14,18 @@ void setup() {
 	}
 }
 
-#define BLACK 0
-#define WHITE 1
 void loop() {
-    // Faz a leitura de todos os sensores e guarda em um vetor
-    for (i; i < 5; i++){
-        sensores[i] = kit.getColor(i);
-    }
 
     // Caso a linha esteja na esquerda
-    if ((sensores[0] == WHITE) && (sensores[4] == BLACK)) {
+    if ((kit.getColor(0) == WHITE) && (kit.getColor(4) == BLACK)) {
         // Acelera mais a roda da direita
-        kit.setSpeed(10, 50); 
-    } 
+        kit.setSpeed(10, 50);
+    }
     // Caso a linha esteja na direita
-    else if ((sensores[0] == BLACK) && (sensores[4] == WHITE)) {
+    else if ((kit.getColor(0) == BLACK) && (kit.getColor(4) == WHITE)) {
         // Acelera mais a roda da esquerda
         kit.setSpeed(50, 10);
-    } 
+    }
     //Caso não leia a linha
     else {
         // Acelera as duas rodas igualmente
